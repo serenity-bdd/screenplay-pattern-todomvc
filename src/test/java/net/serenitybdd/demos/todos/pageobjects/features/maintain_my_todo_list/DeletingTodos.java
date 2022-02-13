e514758c7bed45ac3f1fbe14c2f921c3b2dad785
John Ferguson Smart <john.smart@wakaleo.com>
2022-02-13 09:28:00 +0000

    Fixed a spelling error

--- src/test/java/net/serenitybdd/demos/todos/pageobjects/features/maintain_my_todo_list/DeletingTodos.java
+++ src/test/java/net/serenitybdd/demos/todos/pageobjects/features/maintain_my_todo_list/DeletingTodos.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.WebDriver;
 
 @RunWith(SerenityRunner.class)
 @WithTag("PageObjects pattern")
-public class DeleteingTodos {
+public class DeletingTodos {
 
     @Managed WebDriver driver;
 
