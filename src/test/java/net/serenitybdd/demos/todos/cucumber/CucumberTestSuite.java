99e7115c077325d84db64bd7d4b2dd1643415677
John Ferguson Smart <john.smart@wakaleo.com>
2023-08-01 14:03:04 +0100

    Integrated parallel execution

--- src/test/java/net/serenitybdd/demos/todos/cucumber/CucumberTestSuite.java
+++ src/test/java/net/serenitybdd/demos/todos/cucumber/CucumberTestSuite.java
@@ -1,12 +1,24 @@
 package net.serenitybdd.demos.todos.cucumber;
 
-import io.cucumber.junit.CucumberOptions;
-import net.serenitybdd.cucumber.CucumberWithSerenity;
-import org.junit.runner.RunWith;
+//import io.cucumber.junit.CucumberOptions;
+//import net.serenitybdd.cucumber.CucumberWithSerenity;
+//import org.junit.runner.RunWith;
+//
+//@RunWith(CucumberWithSerenity.class)
+//@CucumberOptions(
+//        features = "src/test/resources/features"
+//)
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(CucumberWithSerenity.class)
-@CucumberOptions(
-        features = "src/test/resources/features"
-)
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("/features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
+                        value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,timeline:target/test-results/timeline")
 public class CucumberTestSuite {
 }
