2fb409f74d938a375927f50238f2ca8c730d4305
John Ferguson Smart <john.smart@wakaleo.com>
2023-10-13 07:57:32 +0100

    Fixed an issue with Screenshots for Screenplay tasks in parallel execution

--- src/test/java/net/serenitybdd/demos/todos/cucumber/CucumberTestSuite.java
+++ src/test/java/net/serenitybdd/demos/todos/cucumber/CucumberTestSuite.java
@@ -10,6 +10,7 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("/features")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,timeline:target/test-results/timeline")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
+                        value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,timeline:target/test-results/timeline")
 public class CucumberTestSuite {
 }
